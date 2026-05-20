4bdc426d9b224751462482d428e74937763be0b0
Juan Pablo Neves <juan_neves1@outlook.com>
2026-05-19 22:51:32 -0300

    Add files via upload

---
+++ Trabalho Receita Grupo/Trabalho Receita Grupo/backend/app.py
@@ -0,0 +1,30 @@
+from flask import Flask
+
+from flask_cors import CORS
+
+from database import DB
+
+from routes.fornecedores import fornecedores_bp
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:1234@localhost:5432/compliance_fornecedores'
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+DB.init_app(app)
+
+CORS(app)
+
+app.register_blueprint(fornecedores_bp)
+
+with app.app_context():
+
+    DB.create_all()
+
+if __name__ == '__main__':
+
+    app.run(host='0.0.0.0', port=5000, debug=True)
+
+#Quando rodar o servidor coloque esse link no navegador"http://127.0.0.1:5000/fornecedor/19131243000197", ele pega da página BrasilAPI 
+# ou se quiser pegar só do banco rode este"http://localhost:5000/fornecedores" se aparecer [],significa que está certo, é pq a tabela do banco não possui dados
